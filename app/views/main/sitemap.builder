00e878e5d529011f3239f5802a8f4e3e7bf843aa
afa <afa.alone@gmail.com>
2010-09-16 13:44:14 +0400

    inital

---
+++ app/views/main/sitemap.builder
@@ -0,0 +1,2 @@
+<h1>Main#sitemap.xml</h1>
+<p>Find me in app/views/main/sitemap.xml.html.erb</p>
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+TeaShop::Application.config.secret_token = '8951991bb80ced71589bcf20dc94747850436071c9e0e19ca6309fd1e1cd7fcb5a2c73c48061368663b731a76dfb0f185a7e237e4fa74e4d068a27b4ebe27a68'
